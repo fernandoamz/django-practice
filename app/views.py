273de83328eeb52f32d2cd93e1443d686213502e
fernando <fernandoamz148@gmail.com>
2019-08-12 12:44:11 -0500

    first commit

---
+++ app/views.py
@@ -0,0 +1,19 @@
+from flask import Blueprint
+from flask import render_template
+
+from .forms import LoginForm
+page = Blueprint('page', __name__)
+
+@page.app_errorhandler(404)
+def page_not_found(error):
+    return render_template('errors/404.html'), 404
+
+@page.route('/')
+def index():
+    return render_template('index.html', title='Index')
+
+@page.route('/login')
+def login():
+    form = LoginForm()
+
+    return render_template('auth/login.html', title='Login', form=form)
